70cc4b8183ca31849f1f8ac9c47f832fbf6707ca
bnudelman-dd <ben.nudelman@datadoghq.com>
2026-08-24 14:38:24 -0600

    more dates debugging12

--- script.js
+++ script.js
@@ -373,8 +373,8 @@ function renderSchedule() {
   });
 
   console.log(GAMES.length);
-  console.log(numDays.innerHTML);
-  numDays.innerHTML = '<p class="numDays" tests" '//GAMES.length + " nights on the calendar. Show up whenever you can make it.</p;
+  console.log(numDays);
+  numDays.innerHTML = '<p class="numDays"> tests</p>'//GAMES.length + " nights on the calendar. Show up whenever you can make it.</p;
 
   const nextIndex = decorated.findIndex((g) => !g.isPast);
 
